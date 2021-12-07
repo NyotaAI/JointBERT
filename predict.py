05e720c88088046130cc5b326e433a3f34b8db1e
Pavel Suchmann <pavel.suchmann@gmail.com>
2021-12-07 18:45:33 +0100

    fixed predict.py issue: model loads from --model_dir instead of a location specified at training time

--- predict.py
+++ predict.py
@@ -26,13 +26,13 @@ def load_model(pred_config, args, device):
         raise Exception("Model doesn't exists! Train first!")
 
     try:
-        model = MODEL_CLASSES[args.model_type][1].from_pretrained(args.model_dir,
+        model = MODEL_CLASSES[args.model_type][1].from_pretrained(pred_config.model_dir,
                                                                   args=args,
                                                                   intent_label_lst=get_intent_labels(args),
                                                                   slot_label_lst=get_slot_labels(args))
         model.to(device)
         model.eval()
-        logger.info("***** Model Loaded *****")
+        logger.info(f"***** Model Loaded: {pred_config.model_dir} *****")
     except:
         raise Exception("Some model files might be missing...")
 
